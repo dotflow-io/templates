5d795b4e326d5cd4213858146a7407d5c25e0743
Fernando Celmer <email@fernandocelmer.com>
2026-04-06 03:14:50 -0300

    ⚙️ FEATURE: Add storage selection (default, file, s3, gcs)

--- {{cookiecutter.project_name}}/{{cookiecutter.module_name}}/workflow.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.module_name}}/workflow.py
@@ -1,11 +1,34 @@
 from dotflow import Config, DotFlow
+{% if cookiecutter.storage == "file" -%}
 from dotflow.providers import StorageFile
-
+{% elif cookiecutter.storage == "s3" -%}
+from dotflow.providers import StorageS3
+{% elif cookiecutter.storage == "gcs" -%}
+from dotflow.providers import StorageGCS
+{% endif %}
 from {{ cookiecutter.module_name }}.actions import step_one, step_two
 
-
+{% if cookiecutter.storage == "default" -%}
+config = Config()
+{% elif cookiecutter.storage == "file" -%}
 config = Config(storage=StorageFile())
-
+{% elif cookiecutter.storage == "s3" -%}
+config = Config(
+    storage=StorageS3(
+        bucket="my-dotflow-bucket",
+        prefix="workflows/",
+        region="us-east-1",
+    )
+)
+{% elif cookiecutter.storage == "gcs" -%}
+config = Config(
+    storage=StorageGCS(
+        bucket="my-dotflow-bucket",
+        prefix="workflows/",
+        project="my-gcp-project",
+    )
+)
+{% endif %}
 
 def main():
     workflow = DotFlow(config=config)
